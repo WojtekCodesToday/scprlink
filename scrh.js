87a76786a75d5662c5706c4e84e6f3b570ae7816
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-05 18:50:15 +0100

    Update scrh.js

--- scrh.js
+++ scrh.js
@@ -41,11 +41,34 @@ waitForElement("#nojs").then(element => {
 // Get the username from the current URL using a regular expression
 let username = window.location.pathname.match(/\/([^\/]+)$/)[1];
 
+function urlExists(url) {
+  // Create a new XMLHttpRequest object
+  let http = new XMLHttpRequest();
+  // Open a HEAD request to the url
+  http.open("HEAD", url, false);
+  // Send the request
+  http.send();
+  // Check the status code
+  if (http.status != 404) {
+    // The page exists
+    return true;
+  } else {
+    // The page does not exist
+    return false;
+  }
+}
+
 // If the username is empty, show an alert and go back
 if (!username) {
   alert("Couldn't extract this account's username.");
   window.history.back();
 } else {
-  // Redirect to the Scratch user page
-  window.location.replace(`https://scratch.mit.edu/users/${username}`);
+  // Validate if the account actually exists
+  if (urlExists(`https://scratch.mit.edu/users/${username}`)) {
+    // Redirect to the Scratch user page
+    window.location.replace(`https://scratch.mit.edu/users/${username}`);
+  } else {
+    alert("This account doesn't exist. It may have been deleted by the Scratch Team.");
+    window.history.back();
+  }
 }
